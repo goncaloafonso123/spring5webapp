b43bd3956a57fdb3704fb75e4b902bdab7a17eff
goncaloafonso123 <86926187+goncaloafonso123@users.noreply.github.com>
2023-01-24 20:47:08 +0000

    24/01

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,81 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstname;
+    private String lastname;
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Author() {
+    }
+
+    public Author(String firstname, String lastname, Set<Book> books) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.books = books;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id == author.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
